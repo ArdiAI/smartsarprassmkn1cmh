260019d13ac71f5d2c7a4cf9f1eeb2853b49c33a
ArdiAI <ardiansyahnugraha485@gmail.com>
2026-07-17 18:30:18 +0700

    Updated config.ts

--- src/brand/config.ts
+++ src/brand/config.ts
@@ -2,7 +2,8 @@ export const brandConfig = {
   system: {
     name: 'SMART SARPRAS',
     fullName: 'Sistem Manajemen Administrasi Ruang Tata Sarana dan Prasarana SMKN 1 Cimahi',
-    tagline: 'Sistem Manajemen Administrasi Ruang Tata Sarana dan Prasarana SMKN 1 Cimahi',
+    tagline: 'Sistem Manajemen Administrasi Ruang Tata 
+      Sarana dan Prasarana SMKN 1 Cimahi',
     shortName: 'SMART SARPRAS',
   },
   school: {
